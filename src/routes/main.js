2ee1496466eef24e5076f231d8183ad2c744d689
Nicz265 <piazza_nicolas@hotmail.com>
2019-12-10 16:05:13 -0300

    Agregado el header en html de registro y carga de producto. Falta arreglarlo para que se vea bien

--- src/routes/main.js
+++ src/routes/main.js
@@ -8,7 +8,7 @@ const mainController = require('../controllers/mainController');
 /* GET - home page. */
 router.get('/', mainController.root);
 router.get('/registro', mainController.registro);
-router.get('/carga-producto', mainController.cargaProducto);
+router.get('/carga', mainController.cargaProducto);
 router.get('/detalle', mainController.detalle);
 
 module.exports = router;
