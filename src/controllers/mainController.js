353e7574dd2662c830355464d1c99ea08ce252df
pdimarcodev <pablo.d.dimarco@gmail.com>
2019-12-28 21:23:42 -0300

    agregados EJS y JSON de productos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,28 +2,33 @@ const fs = require('fs');
 const path = require('path');
 
 // ************ Function to Read an HTML File ************
-function readHTML (fileName) {
-	let filePath = path.join(__dirname, `/../views/${fileName}.html`);
-	let htmlFile = fs.readFileSync(filePath, 'utf-8');
-	return htmlFile;
+// function readHTML (fileName) {
+// 	let filePath = path.join(__dirname, `/../views/${fileName}.html`);
+// 	let htmlFile = fs.readFileSync(filePath, 'utf-8');
+// 	return htmlFile;
+// }
+
+const getProductos =  () => {
+	const url = path.join(__dirname, `/../db/dbProductos.json`);
+	return JSON.parse(fs.readFileSync(url, {encoding: 'utf-8'}));
 }
 
 const controller = {
 	root: (req, res) => {
-		let html = readHTML('index');
-		res.send(html);
+		const productos = getProductos();
+		res.render('index', {productos});
 	},
 	registro: (req, res) => {
-		let html = readHTML('registro');
-		res.send(html);
+		// let html = readHTML('registro');
+		res.render('registro');
 	},
 	cargaProducto: (req, res) => {
-		let html = readHTML('carga');
-		res.send(html);
+		// let html = readHTML('carga');
+		res.render('carga');
 	},
 	detalle: (req, res) => {
-		let html = readHTML('detalle');
-		res.send(html);
+		// let html = readHTML('detalle');
+		res.render('detalle');
 	},
 };
 
