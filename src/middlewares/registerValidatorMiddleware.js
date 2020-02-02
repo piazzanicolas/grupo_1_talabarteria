e8610715143dac8bf2ea0b7ff956ab04881c8918
Nicz265 <piazza_nicolas@hotmail.com>
2020-02-02 09:22:18 -0300

    Terminada la vista de los errores en el formulario de registro

--- src/middlewares/registerValidatorMiddleware.js
+++ src/middlewares/registerValidatorMiddleware.js
@@ -4,8 +4,9 @@ const path = require('path');
 module.exports = [
     check('email').notEmpty().withMessage('El email es necesario').bail().isEmail().withMessage('Dirección de email inválida'),
     check('password').notEmpty().withMessage('Escribí una contraseña').bail().isLength({min:5}).withMessage('La contraseña debe tener más de 5 caracteres'),
-    check('re_password').notEmpty().withMessage('Escribí una contraseña').bail().isLength({min:5}).withMessage('La contraseña debe tener más de 5 caracteres'),
+    check('re_password').notEmpty().withMessage('Repetí la contraseña').bail().isLength({min:5}).withMessage('La contraseña debe tener más de 5 caracteres'),
     check('nombre','El nombre es necesario').notEmpty(),
+    check('nacimiento','La fecha de nacimiento es necesaria').notEmpty(),
     check('apellido','El apellido es necesario').notEmpty(),
     check('telefono','El teléfono es necesario').notEmpty(),
     check('calle','La dirección es necesaria').notEmpty(),
