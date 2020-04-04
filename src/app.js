804c736c723d4783b31ccd51c3e108f79125b41f
pdimarcodev <pablo.d.dimarco@gmail.com>
2020-04-04 16:52:41 -0300

    merge

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 const userCookieMiddleware = require('./middlewares/userCookieMiddleware');
 const localsMiddleware = require('./middlewares/localsMiddleware');
-
+const cors =  require('cors');
 // ************ express() - (don't touch) ************
 const app = express();
 
@@ -17,6 +17,7 @@ app.use(express.static(path.join(__dirname, '../public')));  // Necesario para l
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(session({
